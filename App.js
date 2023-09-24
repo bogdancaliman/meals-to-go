ba9603ccbf53686418fc1c503852629607341d80
Bogdan <bogdan.caliman112@gmail.com>
2023-09-24 17:12:47 +0300

    feat: navbar screens configured

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { Text } from "react-native";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { Ionicons } from "@expo/vector-icons";
 
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme";
@@ -12,12 +13,29 @@ import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const Settings = () => <Text>Settings</Text>;
 const Map = () => <Text>Map</Text>;
 
 const Tab = createBottomTabNavigator();
+
+const TAB_ICON = {
+  Restaurants: "md-restaurant",
+  Map: "md-map",
+  Settings: "md-settings",
+};
+
+const createScreenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+  return {
+    tabBarIcon: ({ size, color }) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+    tabBarActiveTintColor: "tomato",
+    tabBarInactiveTintColor: "gray",
+  };
+};
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -35,11 +53,11 @@ export default function App() {
     <>
       <ThemeProvider theme={theme}>
         <NavigationContainer>
-        <Tab.Navigator>
-        <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
-        <Tab.Screen name="Map" component={Map} />
-        <Tab.Screen name="Settings" component={Settings} />
-      </Tab.Navigator>
+          <Tab.Navigator screenOptions={createScreenOptions}>
+            <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+            <Tab.Screen name="Map" component={Map} />
+            <Tab.Screen name="Settings" component={Settings} />
+          </Tab.Navigator>
         </NavigationContainer>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
