72a5bfe3bce5be4f79c03d2ee82f40bb317e81b6
Bogdan <bogdan.caliman112@gmail.com>
2023-09-27 12:08:17 +0300

    feat: activity-loading-circle added and new geolocation service added

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -3,7 +3,7 @@ import { FlatList } from "react-native";
 
 import styled from "styled-components/native";
 
-import { Searchbar } from "react-native-paper";
+import { Searchbar, ActivityIndicator, MD2Colors } from "react-native-paper";
 
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
@@ -18,11 +18,26 @@ const RestaurantList = styled(FlatList).attrs({
   contentContainerStyle: { padding: 16 },
 })``;
 
+const LoadingContainer = styled.View`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
+
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
+`;
+
 export const RestaurantsScreen = () => {
   const { restaurants, isLoading, error } = useContext(RestaurantsContext);
 
   return (
     <>
+      {isLoading && (
+        <LoadingContainer>
+          <Loading size={50} animating={true} color={MD2Colors.red400} />
+        </LoadingContainer>
+      )}
       <SearchContainer>
         <Searchbar elevation={2} style={{ backgroundColor: "white" }} />
       </SearchContainer>
