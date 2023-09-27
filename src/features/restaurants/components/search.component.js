b5425772814608aa80c152d9ef6a13edf66f3047
Bogdan <bogdan.caliman112@gmail.com>
2023-09-27 13:34:00 +0300

    feat: searching bar is working now

--- src/features/restaurants/components/search.component.js
+++ src/features/restaurants/components/search.component.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { Searchbar } from "react-native-paper";
 import styled from "styled-components/native";
 import { LocationContext } from "../../../services/location/location.context";
@@ -10,6 +10,10 @@ const SearchContainer = styled.View`
 export const Search = () => {
   const { keyword, search } = useContext(LocationContext);
   const [searchKeyword, setSearchKeyword] = useState(keyword);
+
+  useEffect(() => {
+    search(keyword);
+  }, []);
 
   return (
     <SearchContainer>
