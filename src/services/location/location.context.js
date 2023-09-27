366d87451a10cd89e77afd1b1d66780d95afcc7f
Bogdan <bogdan.caliman112@gmail.com>
2023-09-27 12:41:03 +0300

    feat: location context added

---
+++ src/services/location/location.context.js
@@ -0,0 +1,39 @@
+import { createContext, useEffect, useState } from "react";
+
+import { locationRequest, locationTransform } from "./location.service";
+
+export const LocationContext = createContext();
+
+export const LocationContextProvider = ({ children }) => {
+  const [keyword, setKeyword] = useState("san francisco");
+  const [location, setLocation] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const onSearch = (searchKeyword) => {
+    setIsLoading(true);
+    setKeyword(searchKeyword);
+    locationRequest(searchKeyword.toLowerCase())
+      .then(locationTransform)
+      .then((result) => {
+        setLocation(result);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setError(err);
+        setIsLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    onSearch(keyword);
+  }, []);
+
+  return (
+    <LocationContext.Provider
+      value={{ isLoading, error, location, search: onSearch, keyword }}
+    >
+      {children}
+    </LocationContext.Provider>
+  );
+};
